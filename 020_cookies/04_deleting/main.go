08754fd30ddc3d4a08cc17ca38e70bf50ecf5d3d
banobepascal <banobepascal@gmail.com>
2019-05-26 16:35:16 +0300

    initial commit

--- 020_cookies/04_deleting/main.go
+++ 020_cookies/04_deleting/main.go
@@ -6,14 +6,11 @@ import (
 )
 
 func main() {
-
 	http.HandleFunc("/", index)
 	http.HandleFunc("/set", set)
 	http.HandleFunc("/read", read)
 	http.HandleFunc("/expire", expire)
-	http.Handle("/favicon.ico", http.NotFoundHandler())
 	http.ListenAndServe(":8080", nil)
-
 }
 
 func index(w http.ResponseWriter, req *http.Request) {
@@ -22,8 +19,9 @@ func index(w http.ResponseWriter, req *http.Request) {
 
 func set(w http.ResponseWriter, req *http.Request) {
 	http.SetCookie(w, &http.Cookie{
-		Name:  "Session",
+		Name:  "session",
 		Value: "some value",
+		Path: "/",
 	})
 	fmt.Fprintln(w, `<h1><a href="/read">read</a></h1>`)
 }
@@ -34,7 +32,8 @@ func read(w http.ResponseWriter, req *http.Request) {
 		http.Redirect(w, req, "/set", http.StatusSeeOther)
 		return
 	}
-	fmt.Fprintf(w, `<h1>Your Cookie: <br>%v</h1><h1><a href="/expire">expire</a></h1>`, c)
+
+	fmt.Fprintf(w, `<h1>Your Cookie:<br>%v</h1><h1><a href="/expire">expire</a></h1>`, c)
 }
 
 func expire(w http.ResponseWriter, req *http.Request) {
@@ -43,7 +42,9 @@ func expire(w http.ResponseWriter, req *http.Request) {
 		http.Redirect(w, req, "/set", http.StatusSeeOther)
 		return
 	}
-	c.MaxAge = -1
+	c.MaxAge = -1 // delete cookie
 	http.SetCookie(w, c)
 	http.Redirect(w, req, "/", http.StatusSeeOther)
 }
+
+
