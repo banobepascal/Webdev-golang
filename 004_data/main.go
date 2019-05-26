3edec4bd9c92da6ce61cfce801991abbd999c643
banobepascal <banobepascal@gmail.com>
2019-05-26 22:37:11 +0300

    initial commit

--- 004_data/main.go
+++ 004_data/main.go
@@ -9,11 +9,11 @@ import (
 var tpl *template.Template
 
 func init() {
-	tpl = template.Must(template.ParseFiles("index.html"))
+	tpl = template.Must(template.ParseFiles("index.gohtml"))
 }
 
 func main() {
-	err := tpl.ExecuteTemplate(os.Stdout, "index.html", `20 years of age `)
+	err := tpl.ExecuteTemplate(os.Stdout, "index.gohtml", `20 years of age `)
 	if err != nil {
 		log.Fatalln(err)
 	}
