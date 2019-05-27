3a09690233d042a0568fc7e2c69ef7a7edd56345
banobepascal <banobepascal@gmail.com>
2019-05-27 16:49:29 +0300

    initial commit

--- 009_nested_templates/03_define_template/main.go
+++ 009_nested_templates/03_define_template/main.go
@@ -9,20 +9,19 @@ import (
 var tpl *template.Template
 
 func init() {
-	tpl = template.Must(template.ParseGlob("templates/*.html"))
+	tpl = template.Must(template.ParseGlob("templates/*.gohtml"))
 }
 
 func main() {
 
-	err := tpl.ExecuteTemplate(os.Stdout, "index.html", nil)
+	err := tpl.ExecuteTemplate(os.Stdout, "index.gohtml", nil)
 	if err != nil {
 		log.Fatalln(err)
 	}
 
 }
 
-
-// To run html page 
+// To run html page
 // go to terminal and type
 // "go run main.go > file/name/ofchoice.html"
 // file is created and then run the created file
