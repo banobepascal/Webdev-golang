5bebd158ce20b3abc778bba366e0cbe07e02d194
banobepascal <banobepascal@gmail.com>
2019-05-27 15:55:33 +0300

    initial commit

--- 008_pre_global_funcs/01_index/main.go
+++ 008_pre_global_funcs/01_index/main.go
@@ -9,7 +9,7 @@ import (
 var tpl *template.Template
 
 func init() {
-	tpl = template.Must(template.ParseFiles("index.html"))
+	tpl = template.Must(template.ParseFiles("index.gohtml"))
 }
 
 func main() {
