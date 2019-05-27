5cdbcbc40e46fa66fed329df925567712a3167ca
banobepascal <banobepascal@gmail.com>
2019-05-27 16:26:17 +0300

    initial commit

--- 009_nested_templates/01_templates_nested/main.go
+++ 009_nested_templates/01_templates_nested/main.go
@@ -9,12 +9,12 @@ import (
 var tpl *template.Template
 
 func init() {
-	tpl = template.Must(template.ParseGlob("templates/*.html"))
+	tpl = template.Must(template.ParseGlob("templates/*.gohtml"))
 }
 
 func main() {
 
-	err := tpl.ExecuteTemplate(os.Stdout, "index.html", 201)
+	err := tpl.ExecuteTemplate(os.Stdout, "index.gohtml", 201)
 	if err != nil {
 		log.Fatalln(err)
 	}
