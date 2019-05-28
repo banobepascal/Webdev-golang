c00872926b4e743e0f3d3459b20f1fe82d722582
banobepascal <banobepascal@gmail.com>
2019-05-28 19:48:01 +0300

    initial commit

--- 013_understanding_http_package/02_request/02_method/main.go
+++ 013_understanding_http_package/02_request/02_method/main.go
@@ -26,14 +26,14 @@ func (m rolex) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 		req.Form,
 	}
 
-	tpl.ExecuteTemplate(w, "index.html", data)
+	tpl.ExecuteTemplate(w, "index.gohtml", data)
 
 }
 
 var tpl *template.Template
 
 func init() {
-	tpl = template.Must(template.ParseFiles("index.html"))
+	tpl = template.Must(template.ParseFiles("index.gohtml"))
 }
 
 func main() {
