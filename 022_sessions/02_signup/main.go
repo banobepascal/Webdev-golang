de4890f771e63d32507245e2f2c1b3859715fdb7
banobepascal <banobepascal@gmail.com>
2019-05-30 23:37:55 +0300

    good work

---
+++ 022_sessions/02_signup/main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+
+	uuid "github.com/satori/go.uuid"
+)
+
+type user struct {
+	UserName string
+	First    string
+	Last     string
+	Password string
+}
+
+var tpl *template.Template
+var dbUsers = map[string]user{}      // user ID, user
+var dbSessions = map[string]string{} // session ID, user ID
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*"))
+}
+
+func main() {
+	http.HandleFunc("/", index) 
+	http.HandleFunc("/bar", bar)
+	http.HandleFunc("/signup", signup)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":8080", nil)
+}
+
+func index(w http.ResponseWriter, req *http.Request) {
+
+	u := getUser(w, req)
+	tpl.ExecuteTemplate(w, "index.html", u)
+
+}
+
+func bar(w http.ResponseWriter, req *http.Request) {
+	u := getUser(w, req)
+	if !alreadyLoggedIn(req) {
+		http.Redirect(w, req, "/", http.StatusSeeOther)
+		return
+	}
+	tpl.ExecuteTemplate(w, "bar.html", u)
+}
+
+func signup(w http.ResponseWriter, req *http.Request) {
+	if !alreadyLoggedIn(req) {
+		http.Redirect(w, req, "/", http.StatusSeeOther)
+		return
+	}
+
+	//process form submission
+
+	if req.Method == http.MethodPost {
+
+		// get form values
+		un := req.FormValue("username")
+		f := req.FormValue("firstname")
+		l := req.FormValue("lastname")
+		p := req.FormValue("password")
+
+		// username token?
+		if _, ok := dbUsers[un]; ok {
+			http.Error(w, "Username already taken ", http.StatusForbidden)
+			return
+		}
+
+		// create session
+		sID, _ := uuid.NewV4()
+		c := &http.Cookie{
+			Name:  "session",
+			Value: sID.String(),
+		}
+		http.SetCookie(w, c)
+		dbSessions[c.Value] = un
+
+		// store user in dbUsers
+		u := user{un, f, l, p}
+		dbUsers[un] = u
+
+		// redirect
+		http.Redirect(w, req, "/", http.StatusSeeOther)
+		return
+	}
+	tpl.ExecuteTemplate(w, "signup.html", nil)
+
+}
